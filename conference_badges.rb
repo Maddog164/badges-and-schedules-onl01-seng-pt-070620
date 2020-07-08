10eaa5461da3f067f94321645e08fe0611718b18
Denise Henderson <denise@henderson3.us>
2020-07-08 22:22:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,4 +15,5 @@ def assign_rooms(speakers)
   speakers.each_with_index do|name, index|
     room_assign.push("Hello, #{name}! You'll be assigned to room #{{index+1}}")
   end
+  return room_assign
 end
