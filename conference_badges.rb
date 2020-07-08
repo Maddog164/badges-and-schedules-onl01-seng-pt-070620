d641d2dc28f0fdf8863c8360bd95f3feeda91c9e
Denise Henderson <denise@henderson3.us>
2020-07-08 22:33:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,9 @@ speakers=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 def badge_maker(speakers)
   speakers.each do |name|
-    puts "Hello, my name is #{name}."
+    message = "Hello, my name is #{name}."
   end
+  return message
 end
 
 def batch_badge_creator(speakers)
